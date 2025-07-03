f6676ef1b6aa50ceb7be0b2e2536f4a993dd76c9
takumi ogasawara <xiaoliyuanjiang8@gmail.com>
2025-07-03 16:37:39 +0900

    commit

---
+++ Project/Example.lean
@@ -0,0 +1,6 @@
+import Mathlib.Algebra.Group.Subgroup.Pointwise
+import Mathlib.Data.Int.Cast.Lemmas
+import Mathlib.GroupTheory.Congruence.Hom
+import Mathlib.GroupTheory.Coset.Basic
+import Mathlib.GroupTheory.QuotientGroup.Defs
+import Mathlib.Algebra.BigOperators.Group.Finset.Defs
